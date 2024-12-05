ea9cf735e2f03722ec669ec5efe1764badcefe45
mikeberl <berlanda94@gmail.com>
2024-12-05 12:58:32 +0100

    Day 3: Mull It Over

---
+++ day_3/3_1.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+percorso_file = os.path.join(os.getcwd(), 'input.txt')
+
+result = 0
+    
+with open(percorso_file, 'r') as file:
+    content = file.read()
+    #parsing
+    multiplications = re.findall(r'mul\((\d+),(\d+)\)', content)
+    
+    # summing all multiplications
+    for x, y in multiplications:
+        result += int(x) * int(y)
+
+print(f"Sum of multiplications: {result}")
+
