7f45b76e7ff3b8dbe29a34bc5ccbfe0f1f06d84d
mikeberl <berlanda94@gmail.com>
2024-12-05 12:26:14 +0100

    Day 1: Historian Hysteria

---
+++ day_1/1_1.py
@@ -0,0 +1,22 @@
+import os
+
+percorso_file = os.path.join(os.getcwd(), 'input.txt')
+
+first_column = []
+second_column = []
+sum = 0
+
+with open(percorso_file, 'r') as file:
+    for line in file:
+        num1, num2 = map(int, line.strip().split())
+        first_column.append(num1)
+        second_column.append(num2)
+
+# Order array
+first_column.sort()
+second_column.sort()
+
+for i in range(len(first_column)):
+    sum += abs(first_column[i] - second_column[i])
+
+print(f'The sum of the difference between the numbers of the first and second column in ascending order: {sum}')
